811e026fc26c4dc99e22b66acedfd93d283fac58
steffehj <steffehj@hiof.no>
2024-01-26 12:07:23 +0100

    Changed and added JS and CSS

--- oblig2script.js
+++ oblig2script.js
@@ -1,32 +1,124 @@
-console.log(resources)
+// Lager knapper for hver kategori inne i ressurser.js filen. 
+function createButtons() {
+    const buttonContainer = document.getElementById('btns');
 
-let resouceHTML = ""
+    resources.forEach((resource, index) => {
+      const button = document.createElement('button');
+      const categoryId = `button-${index}`; 
 
-resources.map(resouce => resouceHTML += `
-<button id="${resouce.category}-btn" class="btn"> ${resouce.category} </button> 
+      button.textContent = resource.category;
+      button.id = categoryId;
+      button.className = "btn";
 
-`)
+      button.addEventListener('click', () => {
+        // Kaller funksjoner for knappen. 
+        handleButtonClick(index);
 
-console.log(resouceHTML)
+      });
 
-document.getElementById("btns").innerHTML = resouceHTML;
+      buttonContainer.appendChild(button);
+    });
+  }
+
+  // -----------------------------------------------------------------------------------------------------------
+
+  // Funksjonalitet for knapper. 
+
+    function handleButtonClick(index) {
+    
+    // Sjekker at knappene fungerer. 
+    console.log(`Button ${index} clicked`)
+
+    updateContentTransform(index);
+
+    changeButtonColor(index);
+    }
+
+// -----------------------------------------------------------------------------------------------------------
+
+  // En funksjon som endrer translateX verdien for hvert innhold, slik at den forsvinner fra siden. 
+
+    function updateContentTransform(index) {
+    const divContainer = document.getElementById('content-container');
+  
+    const contentDivs = divContainer.children;
+  
+    for (let i = 0; i < contentDivs.length; i++) {
+      const contentDiv = contentDivs[i];
+      const translateXValue = i === index ? '0%' : '300%';
+      contentDiv.style.transform = `translateX(${translateXValue})`;
+    }
+  }
+
+// Endrer fargen for fanen som blir trykket på. 
+
+  function changeButtonColor(index) {
+    const buttonContainer = document.getElementById('btns');
+  
+    const buttons = buttonContainer.children;
+  
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+      button.style.backgroundColor = i === index ? '#fff' : '#2A324B'; 
+      button.style.color = i === index ? '#2A324B' : '#fff' 
+    }
+  }
+
+// ------------------------------------------------------------------------------------------------------------
 
 // Legger inn innhold for hver meny i fanemenyen. 
 
-let contentHTML = ""
+function createDivs() {
+    const contentContainer = document.getElementById('content-container');
 
-resources.map(resouce => contentHTML += `<div id="${resouce.category}" class="content ${resouce.category}">
-<div class="content-left">
-<h1>${resouce.category}</h1> 
-<p>${resouce.text}</p>
-<a href="#">${resouce.sources}</a>
-</div>
-</div>`)
+    // Lagrer all innhold fra ressurser.js inne i nye <div>, og legger til slutt den i <content-container> i html. 
+    resources.forEach((resource, index) => {
+        const contentId = `category-div-${index}`; 
 
-console.log(contentHTML)
+        const div = document.createElement('div');
+        const heading = document.createElement('h2');
+        const paragraph = document.createElement('p');
 
-var div = document.getElementById("content");
-div.innerHTML += contentHTML
+        div.id = contentId;
+        div.className = "content";
+        heading.textContent = resource.category;
+        paragraph.textContent = resource.text;
 
-//document.getElementById("content").innerHTML = contentHTML;
+        div.appendChild(heading);
+        div.appendChild(paragraph);
 
+        // Lager en <ul> og legger til en <li> for hver link. 
+        const ul = document.createElement('ul');
+
+        div.appendChild(ul);
+
+        resource.sources.forEach((source, sourceIndex) => {
+            const li = document.createElement('li')
+            li.className = "list-elements";
+            
+            const link = document.createElement('a');
+            link.textContent = `${source.title}`;
+            link.href = source.url;
+            link.target = "_blank"; 
+
+            const br = document.createElement('br');
+
+            li.appendChild(link);
+            li.appendChild(br);
+
+            ul.appendChild(li);
+        });
+        contentContainer.appendChild(div);
+    });
+}
+
+// -------------------------------------------------------------------------------------------------------------
+
+function initializePage() {
+    createButtons();
+    createDivs();
+  }
+  
+  window.onload = initializePage;
+
+// -------------------------------------------------------------------------------------------------------------
